594e36d04326298232899527106399ab5656983c
Sergey Chizhov <sirigu07@gmail.com>
2025-03-16 19:00:13 +0300

    3_Часть_второй_коммит

--- src/test/java/test/ui/ConstructorPartTest.java
+++ src/test/java/test/ui/ConstructorPartTest.java
@@ -2,10 +2,11 @@ package test.ui;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import io.qameta.allure.Step;
 import pages.MainPage;
 
 @Epic("Навигация в разделе конструктора") // Эпик для Allure-отчётов
@@ -14,7 +15,7 @@ public class ConstructorPartTest extends TestsSetUp {
     private MainPage mainPage; // Главная страница
 
     @Before
-    @Step("Инициализация тестовой среды")
+    @Step("Подготовка тестовой среды")
     @Description("Выполняет инициализацию тестовой среды перед началом тестов")
     public void setUp() {
         super.setUp();
@@ -23,21 +24,24 @@ public class ConstructorPartTest extends TestsSetUp {
     }
 
     @Test
-    @Step("Тест: Переключение на вкладку 'Булки'")
+    @DisplayName("Переключение на вкладку 'Булки'")
+    @Description("Проверяет, что после нажатия на вкладку 'Булки' она становится активной")
     public void testSwitchToBunsTab() {
         mainPage.clickTabWithWait("Булки");
         Assert.assertTrue("Вкладка 'Булки' не активна", mainPage.isCorrectTabActive(0));
     }
 
     @Test
-    @Step("Тест: Переключение на вкладку 'Соусы'")
+    @DisplayName("Переключение на вкладку 'Соусы'")
+    @Description("Проверяет, что после нажатия на вкладку 'Соусы' она становится активной")
     public void testSwitchToSaucesTab() {
         mainPage.clickTabWithWait("Соусы");
         Assert.assertTrue("Вкладка 'Соусы' не активна", mainPage.isCorrectTabActive(1));
     }
 
     @Test
-    @Step("Тест: Переключение на вкладку 'Начинки'")
+    @DisplayName("Переключение на вкладку 'Начинки'")
+    @Description("Проверяет, что после нажатия на вкладку 'Начинки' она становится активной")
     public void testSwitchToFillingsTab() {
         mainPage.clickTabWithWait("Начинки");
         Assert.assertTrue("Вкладка 'Начинки' не активна", mainPage.isCorrectTabActive(2));
